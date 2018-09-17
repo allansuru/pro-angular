4ffd8614c0c1283882c024bb735a61714f13af21
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-09-17 20:29:38 -0300

    Using ng-content with projection slots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,18 @@ import { User } from './auth-form/auth-form.interface';
     <div>
       <auth-form
         (submitted)="createUser($event)">
-        <h3>Create account</h3>
+        <h3>Create account 2</h3>
+        <button type="submit">
+        Join us
+      </button>
       </auth-form>
       <auth-form
         (submitted)="loginUser($event)">
         <h3>Login</h3>
+        <button type="submit">
+        Login
+      </button>
       </auth-form>
-      <auth-form
-      (submitted)="testSubmit($event)">
-      <h3>Test</h3>
-    </auth-form>
   </div>
 
   `
@@ -31,9 +33,4 @@ export class AppComponent {
   loginUser(user: User) {
     console.log('Login', user);
   }
-
-  testSubmit(user: User) {
-    console.log('Other Submit', user);
-  }
-
 }
