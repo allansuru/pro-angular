cabbb4b674632fb99c466a5da5dc86ae356e0f21
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-09-27 19:59:59 -0300

    Using ngTemplateOutlet with context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,19 @@ import { User } from './auth-form/auth-form.interface';
   template: `
     <div>
       <ng-container
-        [ngTemplateOutlet]="tmpl">
+        [ngTemplateOutlet]="tmpl"
+        [ngTemplateOutletContext]="ctx">
       </ng-container>
-      <ng-template #tmpl>
-        Allan Passos : Brasil, BR
+      <ng-template #tmpl let-name let-location="location">
+        {{ name }} : {{ location }}
       </ng-template>
     </div>
   `
 })
-export class AppComponent {
 
+export class AppComponent {
+  ctx = {
+    $implicit: 'Allan Passos',
+    location: 'Brasil, BR'
+  };
 }
