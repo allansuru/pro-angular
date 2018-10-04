246ee3af0df1ed0599e669c798f74b5374eee4c1
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-04 19:37:19 -0300

    React forms SETUP!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FileSizePipe } from './filesize.pipe';
+import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FileSizePipe
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StockInventoryModule
   ],
   bootstrap: [
     AppComponent
