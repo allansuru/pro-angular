e4721bc3b685377014a5c5e7c221ce15362c8c98
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-08 20:27:38 -0300

    FormGroup collections with FormArray

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -40,11 +40,11 @@ import { Product } from '../../models/product.interface';
 export class StockInventoryComponent {
 
   products: Product[] = [
-    { 'id': 1, 'price': 2800, 'name': 'MacBook Pro' },
-    { 'id': 2, 'price': 50, 'name': 'USB-C Adaptor' },
-    { 'id': 3, 'price': 400, 'name': 'iPod' },
-    { 'id': 4, 'price': 900, 'name': 'iPhone' },
-    { 'id': 5, 'price': 600, 'name': 'Apple Watch' },
+    { "id": 1, "price": 2800, "name": "MacBook Pro" },
+    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
+    { "id": 3, "price": 400, "name": "iPod" },
+    { "id": 4, "price": 900, "name": "iPhone" },
+    { "id": 5, "price": 600, "name": "Apple Watch" },
   ];
 
   form = new FormGroup({
@@ -52,12 +52,20 @@ export class StockInventoryComponent {
       branch: new FormControl(''),
       code: new FormControl('')
     }),
-    selector: new FormGroup({
-      product_id: new FormControl(''),
-      quantity: new FormControl(10)
-    }),
-    stock: new FormArray([])
+    selector: this.createStock({}),
+    stock: new FormArray([
+      this.createStock({ product_id: 1, quantity: 10 }),
+      this.createStock({ product_id: 3, quantity: 50 }),
+      this.createStock({ product_id: 66, quantity: 150 }),
+    ])
   })
+
+  createStock(stock) {
+    return new FormGroup({
+      product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
+      quantity: new FormControl(stock.quantity || 10)
+    });
+  }
 
   onSubmit() {
     console.log('Submit:', this.form.value);
