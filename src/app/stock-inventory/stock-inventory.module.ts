29c8631fbc4d37daa7254aa05ce0bb594d67b78b
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-15 20:41:59 -0300

    Custom form control base

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -7,6 +7,7 @@ import { StockInventoryComponent } from './containers/stock-inventory/stock-inve
 import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
 import { StockProductsComponent } from './components/stock-products/stock-products.component';
 import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
+import { StockCounterComponent } from './components/stock-counter/stock-counter.component';
 import { StockInventoryService } from './services/stock-inventory.service';
 import { HttpModule } from '@angular/http';
 
@@ -15,7 +16,8 @@ import { HttpModule } from '@angular/http';
     StockInventoryComponent,
     StockBranchComponent,
     StockProductsComponent,
-    StockSelectorComponent
+    StockSelectorComponent,
+    StockCounterComponent
   ],
   imports: [
     CommonModule,
