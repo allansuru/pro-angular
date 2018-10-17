aaac8ab300fce3f2d49eacd2a78ff2848b9768dd
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-17 14:08:55 -0300

    Validators object for FormControls

--- src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
+++ src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,  OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -11,21 +11,32 @@ import { FormGroup } from '@angular/forms';
           type="text" 
           placeholder="Branch ID"
           formControlName="branch">
+        <div
+          class="error" 
+          *ngIf="required('branch')">
+          Branch ID is required
+        </div>
         <input 
           type="text" 
           placeholder="Manager Code"
           formControlName="code">
+        <div
+          class="error" 
+          *ngIf="required('code')">
+          Manager ID is required
+        </div>
       </div>
     </div>
   `
 })
-export class StockBranchComponent implements OnInit
-{
+export class StockBranchComponent {
   @Input()
   parent: FormGroup;
 
-  ngOnInit() {
-    console.log('Parent: ', this.parent);
+  required(name: string) {
+    return (
+      this.parent.get(`store.${name}`).hasError('required') && 
+      this.parent.get(`store.${name}`).touched
+    );
   }
-
 }
