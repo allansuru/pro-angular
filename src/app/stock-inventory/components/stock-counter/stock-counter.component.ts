4b7efe0dbb4cec0f93b98365159b7af93ac6335a
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-10-17 13:34:38 -0300

    Adding keyboard events to our control

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -12,18 +12,24 @@ const COUNTER_CONTROL_ACCESSOR = {
   providers: [COUNTER_CONTROL_ACCESSOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
-    <div class="stock-counter">
+    <div
+      class="stock-counter"
+      [class.focused]="focus">
       <div>
-        <div>
+        <div
+         tabindex="0"
+         (keydown)="onKeyDown($event)"
+         (blur)="onBlur($event)"
+         (focus)="onFocus($event)">
           <p>{{ value }}</p>
           <div>
-            <button 
+            <button
               type="button"
               (click)="increment()"
               [disabled]="value === max">
               +
             </button>
-            <button 
+            <button
               type="button"
               (click)="decrement()"
               [disabled]="value === min">
@@ -43,14 +49,12 @@ export class StockCounterComponent implements ControlValueAccessor {
   registerOnTouched(fn) {
     this.onTouch = fn;
   }
-  
+
   registerOnChange(fn) {
     this.onModelChange = fn;
   }
-  
+
   writeValue(value) {
-      console.log('writevalue: ', value);
-      
     this.value = value || 0;
   }
 
@@ -59,6 +63,36 @@ export class StockCounterComponent implements ControlValueAccessor {
   @Input() max: number = 1000;
 
   value: number = 10;
+
+  focus: boolean;
+
+  onKeyDown(event: KeyboardEvent) {
+
+    const handlers = {
+      ArrowDown: () => this.decrement(),
+      ArrowUp: () => this.increment()
+    };
+
+    if (handlers[event.code]) {
+      handlers[event.code]();
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.onTouch();
+  }
+
+  onBlur(event: FocusEvent) {
+    this.focus = false;
+    event.preventDefault();
+    event.stopPropagation();
+    this.onTouch();
+  }
+  onFocus(event: FocusEvent) {
+    this.focus = true;
+    event.preventDefault();
+    event.stopPropagation();
+    this.onTouch();
+  }
 
   increment() {
     if (this.value < this.max) {
