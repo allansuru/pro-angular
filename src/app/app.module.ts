45ad5baddce4509dd9d768b84f1848b57c622a84
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-12-15 18:02:28 -0200

    Configurable NgModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { PizzaViewerComponent } from './containers/pizza-viewer.component';
-import { DrinkViewerComponent } from './containers/drink-viewer.component';
-import { SideViewerComponent } from './containers/side-viewer.component';
-
+import { FoodStoreModule } from './food-store/food-store.module';
 import { AppComponent } from './app.component';
-import { API_TOKEN } from './token';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PizzaViewerComponent,
-    DrinkViewerComponent,
-    SideViewerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FoodStoreModule.forRoot({
+      storeId: 10292,
+      storeToken: 'eca938c99a0e9ff91029dc'
+    })
   ],
   bootstrap: [
     AppComponent
-  ],
-  providers: [
-    { provide: API_TOKEN, useValue: 'http://localhost:3000/pizzas' }
   ]
 })
 export class AppModule {}
